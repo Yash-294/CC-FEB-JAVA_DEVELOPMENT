8d8557766d6266fd516a64456bf4736f8c59b357
Yash Shinde <90593348+Yash-294@users.noreply.github.com>
2023-02-26 00:16:53 +0530

    Delete TASK2-LIBRARY_BOOK_ISSUE_AND_RETURN_SYSTEM/librarybookissue directory

--- TASK2-LIBRARY_BOOK_ISSUE_AND_RETURN_SYSTEM/librarybookissue/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.codeclausetask2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires transitive javafx.graphics;
-    requires transitive java.sql;
-    
-    opens com.codeclausetask2 to javafx.fxml;
-    exports com.codeclausetask2;
-}
